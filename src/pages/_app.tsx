140e1b74f1a836d4b60a4902d60891bea72b8df5
hieudoanm <hieumdoan@gmail.com>
2025-02-04 17:29:55 +0700

    move to page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import '@broca-wernicke/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Geist_Mono } from 'next/font/google';
+import Head from 'next/head';
+
+const mono = Geist_Mono({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Broca X Wernicke</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
+      <div className={mono.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+}

0665f762a39d0ecc9f6f7ee28fcb30a02681eebd
hieudoanm <hieumdoan@gmail.com>
2025-02-08 20:50:55 +0700

    update packages

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  distDir: '../docs',
+  distDir: '../../docs',
 };
 
 export default nextConfig;

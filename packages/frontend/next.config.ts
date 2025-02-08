9ed8a11a2446a6ac69004da21a31d488777bb5b9
hieudoanm <hieumdoan@gmail.com>
2025-02-08 21:36:51 +0700

    Update docs

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -5,7 +5,12 @@ const NODE_ENV = process.env.NODE_ENV ?? 'development';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  reactStrictMode: true,
+  trailingSlash: true,
+  experimental: { turbo: { treeShaking: true } },
+  basePath: NODE_ENV === 'development' ? '' : '/broca',
   distDir: NODE_ENV === 'development' ? '.next' : '../../docs',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;

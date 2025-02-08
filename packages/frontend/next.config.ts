1d278942295bb54cfe29e46f866de342c486b65c
hieudoanm <hieumdoan@gmail.com>
2025-02-08 21:33:56 +0700

    Update LLM

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from 'next';
 
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  distDir: '../../docs',
+  distDir: NODE_ENV === 'development' ? '.next' : '../../docs',
 };
 
 export default nextConfig;
